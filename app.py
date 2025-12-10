18c44938f14f30a3a9210185bd3b17e5fa0373c8
Thaadshaayani Rasanehru <thaadshaayanirasanehru@gmail.com>
2025-12-10 14:00:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ def render_songs(tracks_df):
             display_df,
             use_container_width=True,
             hide_index=True,
-            height=390,
+            height=320,
             column_config={
                 "Song": st.column_config.TextColumn("Song", width="medium"),
                 "Artist": st.column_config.TextColumn("Artist", width="small"),
@@ -393,7 +393,7 @@ def render_artists(tracks_df, artists_df):
             display_df[["Artist", "Tracks", "Avg Pop", "Followers"]],
             use_container_width=True,
             hide_index=True,
-            height=390
+            height=320
         )
     
     with col2:
